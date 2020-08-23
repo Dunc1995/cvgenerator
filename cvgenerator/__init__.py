70720dfee50ff7de4bbb95a3ac674dadfab9f78d
Dunc1995 <dpbailey1995@outlook.com>
2020-08-23 14:03:03 +0100

    hurting my head

--- cvgenerator/__init__.py
+++ cvgenerator/__init__.py
@@ -3,6 +3,7 @@ ROOT_DIRECTORY = 'cv_bin'
 #endregion
 
 #region #? Stateful
+SCHEMAS = []
 IS_EXITED = False
 DB_CLIENT = None
 #endregion
--- cvgenerator/parser.py
+++ cvgenerator/parser.py
@@ -2,6 +2,9 @@ import yaml
 import json
 import importlib.resources as pkg_resources
 from . import templates  # relative-import the *package* containing the templates
+import cvgenerator as cv
+
+SCHEMA_INDEX = []
 
 def get_schema_template():
     return pkg_resources.read_text(templates, 'schemas_hierarchy.yaml')
@@ -23,20 +26,28 @@ def get_schema_dict_from_file():
     
     i = 0
     while keep_iterating == True:
-        print('''
-        --------
-        Level {}
-        --------
-        '''.format(i))
-        
+        # print('''
+        # --------
+        # Level {}
+        # --------
+        # '''.format(i))
         children_values = []
+        j = 0
+
         for value in values_array:
-            sub_values = get_children_keys(value)
+            sub_values, sub_keys, j = get_children_keys(j, value)
             for sub_val in sub_values:
                 children_values.append(sub_val)
+            if not sub_keys == None:
+                SCHEMA_INDEX.append({ i: sub_keys })
+            j += 1
+
         keep_iterating = nested_keys_exist(values_array)
         values_array = children_values
         i += 1
+
+    for entry in SCHEMA_INDEX:
+        print(str(entry))
 
 def nested_keys_exist(value_array: list):
     '''
@@ -49,7 +60,7 @@ def nested_keys_exist(value_array: list):
             break
     return output
 
-def get_children_keys(input_obj):
+def get_children_keys(index: int, input_obj):
     '''
     Constructs a list of child keys for further processing.
     '''
@@ -57,26 +68,56 @@ def get_children_keys(input_obj):
     #!This is is likely to be problematic
     #!-----------------------------------
     output = []
+    output_keys = None
     try:
-        if isinstance(input_obj, dict):
-            for key, value in input_obj.items():
-                print(key)
-                if not value == None:
-                    output.append(value)
-                else:
-                    output.append(key)
+        keys = __get_child_keys_if_dict(output, input_obj)
+        if not keys == None:
+            output_keys = {index: keys}
         elif isinstance(input_obj, list):
+            # print('v--list--v')
+            keys_group = []
             for ob in input_obj:
-                if isinstance(ob, dict): #TODO could put this if statement into its own method to avoid code repetition
-                    for key, value in ob.items():
-                        print(key)
-                        if not value == None:
-                            output.append(value)
-                        else:
-                            output.append(key)
-        else:
-            pass
-            #print(str(input_obj))
+                sub_keys = __get_child_keys_if_dict(output, ob)
+                for key in sub_keys:
+                    keys_group.append(key)
+            output_keys = {index: keys_group}
+            # print('^--------^')
     except Exception as e:
         print(str(e))
-    return output
+    return output, output_keys, index
+
+def __get_child_keys_if_dict(input_list: list, instance):
+    child_keys = None
+    if isinstance(instance, dict):
+        child_keys = []
+        for key, value in instance.items():
+            # print(key)
+            child_keys.append(key)
+            if not value == None:
+                input_list.append(value)
+            else:
+                input_list.append(key)
+        # print('---children---')
+        # for entry in child_keys:
+        #     print(entry)
+        # print('--------------')
+
+    return child_keys
+
+def get_default_parent_schema():
+    return { 
+        'type': None,
+        'name': None,
+        'parent': None,
+        'children': [],
+        'tags': []
+        }
+
+def get_default_schema():
+    return { 
+        'type': None,
+        'name': None,
+        'value': None,
+        'parent': None,
+        'tags': []
+        }
